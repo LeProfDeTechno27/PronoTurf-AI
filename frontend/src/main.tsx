3b01b976c32ba7bb6caa27198c4bb61dd2766506
LeProfDeTechno27 <jeremy.bruerre@free.fr>
2025-11-14 19:36:04 +0100

    fix: ensure login route accessible

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 import './index.css'
@@ -22,10 +22,10 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
